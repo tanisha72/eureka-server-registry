49272caea38455f8679e20077c2a9b34441b662a
Tanisha Gupta <tanishagupta72@gmail.com>
2023-04-03 05:52:50 +0530

    Initial commit

---
+++ src/test/java/com/demo/eurekaserverregistry/EurekaServerRegistryApplicationTests.java
@@ -0,0 +1,13 @@
+package com.demo.eurekaserverregistry;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EurekaServerRegistryApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
